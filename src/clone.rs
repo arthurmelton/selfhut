5c92aea0776521efb6e29f39d41d3271f00a2289
Arthur Melton <me@arthurmelton.com>
2022-11-20 13:49:43 -0500

    undo that change

--- src/clone.rs
+++ src/clone.rs
@@ -7,6 +7,7 @@ use std::path::Path;
 pub async fn clone(repo: String, path: PathBufWithDotfiles) -> Option<NamedFile> {
     let mut repo_path = CONFIG.git_location.clone();
     repo_path.push(format!("{}.git", repo));
+    repo_path.push(".git");
     repo_path.push(path.get());
     NamedFile::open(repo_path).await.ok()
 }
